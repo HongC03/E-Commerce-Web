af5de351158520baceee6356d1e542ef82da789d
HongC03 <86474083+HongC03@users.noreply.github.com>
2021-08-28 18:20:11 +0800

    add some css to the home page

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,11 @@ import { services } from '../utils/constants';
 const Services = () => {
 	return (
 		<Wrapper>
-			<div className='section-center'>
+			<div className='core-value-center'>
+			<div className='title'>
+				<h2 className='core-value'>Core Values</h2>
+				<div className='underline'></div>
+			</div>
 				<div className='services-center'>
 					{services.map((service) => {
 						const { id, title, text } = service;
@@ -22,28 +26,36 @@ const Services = () => {
 };
 
 const Wrapper = styled.section`
+	margin-top: 100px;
 	margin-bottom: 200px;
 	h3,
 	h4 {
 		color: var(--clr-primary-1);
 	}
-	padding: 5rem 0;
+	padding: 2.5rem 2.5rem;
 
 	background: var(--clr-grey-10);
 
-	.header h3 {
-		margin-bottom: 2rem;
-	}
 	p {
 		margin-bottom: 0;
 		line-height: 1.8;
 		color: var(--clr-primary-3);
+	}
+	.core-value-center {
+  width: 90vw;
+  margin: 0 auto;
+  max-width: var(--max-width);
+	padding-bottom: 100px;
+	}	
+	.core-value {
+		padding-top: 75px;
 	}
 	.services-center {
 		margin-top: 4rem;
 		display: grid;
 		gap: 2.5rem;
 	}
+
 	.service {
 		background: var(--clr-primary-10);
 		text-align: center;
@@ -51,20 +63,6 @@ const Wrapper = styled.section`
 		border-radius: var(--radius);
 		p {
 			color: var(--clr-primary-2);
-		}
-	}
-	span {
-		width: 4rem;
-		height: 4rem;
-		display: grid;
-		margin: 0 auto;
-		place-items: center;
-		margin-bottom: 1rem;
-		border-radius: 50%;
-		background: var(--clr-primary-10);
-		color: var(--clr-primary-1);
-		svg {
-			font-size: 2rem;
 		}
 	}
 	@media (min-width: 992px) {
@@ -81,7 +79,7 @@ const Wrapper = styled.section`
 	@media (min-width: 1280px) {
 		padding: 0;
 		.section-center {
-			transform: translateY(5rem);
+			transform: translateY(-1rem);
 		}
 	}
 `;
